220195b7719ef1a9ec5c2c60781ff762f8423eba
Dikshant Adhikari <ad.dikshant@gmail.com>
2022-08-18 02:08:52 -0400

    fix: bash syntax for checking not null

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,7 +51,7 @@ if [[ -z "$LOCAL_SUBNETS" && "$LOCAL_SUBNET" ]]; then
 fi
 
 # Hack to allow upstream port forwarding through a VPN provider
-if [[ -z "$PORT_FORWARD_DEST" && "$PORT_FORWARD_TO" ]]; then
+if [[ -n "$PORT_FORWARD_DEST" && "$PORT_FORWARD_TO" ]]; then
     echo "Doing port forward from port ${PORT_FORWARD_TO} to destination port ${PORT_FORWARD_DEST}" >&2
     iptables -t nat -I PREROUTING -p tcp --dport $PORT_FORWARD_DEST -j REDIRECT --to $PORT_FORWARD_TO
 fi
